7ad08224d8a49a8d51d5f3473e0019f61f98808f
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-07-06 20:04:17 +0530

    feat: remove hackathons section and hide blog nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -136,11 +136,7 @@ export default function Page() {
           <ProjectsSection />
         </BlurFade>
       </section>
-      <section id="hackathons">
-        <BlurFade delay={BLUR_FADE_DELAY * 13}>
-          <HackathonsSection />
-        </BlurFade>
-      </section>
+      {/* Hackathons section - removed */}
       <section id="contact">
         <BlurFade delay={BLUR_FADE_DELAY * 16}>
           <ContactSection />
